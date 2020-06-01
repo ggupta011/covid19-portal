54487ba5f969a14d5b7246df32d4a761b11ad44b
ggupta011 <ggupta011@gmail.com>
2020-06-01 14:23:26 +0200

    Updates

---
+++ precache-manifest.b42d531e176a4a0ac36c7506a704ef3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f6d613303350c1da67f5d9ec0d02db",
+    "url": "/covid19-portal/index.html"
+  },
+  {
+    "revision": "7cd9d6c3a4fe6976b4b1",
+    "url": "/covid19-portal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "541e61d507cc13c500ef",
+    "url": "/covid19-portal/static/js/2.3d1b1a9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-portal/static/js/2.3d1b1a9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd9d6c3a4fe6976b4b1",
+    "url": "/covid19-portal/static/js/main.4256f472.chunk.js"
+  },
+  {
+    "revision": "7767d542abd6d75561c4",
+    "url": "/covid19-portal/static/js/runtime-main.b9be2b71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
